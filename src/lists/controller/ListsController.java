38587674077ca1da1ece7d05c1874dedcf470587
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-10-05 13:26:41 -0600

    Makes every letter have it's own thingy

--- src/lists/controller/ListsController.java
+++ src/lists/controller/ListsController.java
@@ -45,6 +45,11 @@ public class ListsController
 				{
 					popup.displayText(currentCreator.substring(currentLetterIndex, currentLetterIndex + 1));
 				}
+				String topic = currentKahoot.getTopic();
+				for (int letter = currentKahoot.getTopic().length() - 1; letter >= 0; letter -= 1)
+				{
+					popup.displayText(topic.substring(letter, letter + 1));
+				}
 			}
 		}
 		/** Fills list of myKahoots
--- src/lists/model/Kahoot.java
+++ src/lists/model/Kahoot.java
@@ -35,4 +35,9 @@ public class Kahoot
 	{
 		return creator;
 	}
+	
+	public String getTopic()
+	{
+		return topic;
+	}
 }
